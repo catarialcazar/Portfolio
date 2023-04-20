001d15631f4a55fb3ff31ededd4149cc02604ce7
Catari <89741808+catarialcazar@users.noreply.github.com>
2023-04-20 16:50:51 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 // // It ain't much, but it's honest work:
 var w = document.documentElement.clientWidth || window.innerWidth;
 const hidepc = document.querySelector(".hidepc");
-const loadingcontainer = document.querySelector(".loadingcontainer");
 const main = document.querySelector("main");
 
 window.onload = function () {
@@ -9,16 +8,11 @@ window.onload = function () {
 };
 
 function init() {
-    setTimeout(() => {
-        loadingcontainer.style.opacity = 0;
-        loadingcontainer.style.display = "none";
-
-        if (w <= 680) {
-            hidepc.style.display = "flex";
-            setTimeout(() => (hidepc.style.opacity = 1), 16);
-        } else {
-            main.style.display = "block";
-            setTimeout(() => (main.style.opacity = 1), 16);
-        }
-    }, 2000);
+    if (w <= 680) {
+        hidepc.style.display = "flex";
+        setTimeout(() => (hidepc.style.opacity = 1), 16);
+    } else {
+        main.style.display = "block";
+        setTimeout(() => (main.style.opacity = 1), 16);
+    }
 }
